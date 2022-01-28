05af6d279578215663b360469192bee0bbd3a666
Nay Thway <naythway281997@gmail.com>
2022-01-28 11:40:12 +0630

    Add project files.

---
+++ MovieApiI/Data/MovieApiIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieAPI.Models;
+
+namespace MovieApiI.Data
+{
+    public class MovieApiIContext : DbContext
+    {
+        public MovieApiIContext (DbContextOptions<MovieApiIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieAPI.Models.Movie> Movie { get; set; }
+    }
+}
